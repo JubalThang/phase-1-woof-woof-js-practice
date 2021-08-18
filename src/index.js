9bd3b4a4ef2ca822b037e7ee28d99e625c345356
jubalthang <jubalthang@gamil.com>
2021-08-18 14:13:09 -0700

    completed challange

--- src/index.js
+++ src/index.js
@@ -1,23 +1,35 @@
 
 let pupsContainer
 let pupContainer
+let filterOn = false 
 document.addEventListener('DOMContentLoaded', event => {
     pupsContainer = document.getElementById('dog-bar')
     pupContainer = document.getElementById('dog-info')
-    // document.getElementById('good-dog-filter').addEventListener(event => {
-    //     event.target.textContent = 
-    //     fetchPups(filterOn = true)
-    // })
-    fetchPups()
+    document.getElementById('good-dog-filter').addEventListener('click',event => {
+        filterContextToggle(event)
+        filterOn = filterOn ? false : true
+        document.getElementById('dog-bar').innerHTML = ''
+        fetchPups(filterOn)
+    })
+    
+   fetchPups(filterOn)
 })
 
-function fetchPups(filterOn = false) {
+function filterContextToggle(e) {
+    if (e.target.textContent === 'Filter good dogs: OFF') {
+        document.getElementById('good-dog-filter').textContent = 'Filter good dogs: ON'
+    } else {
+        document.getElementById('good-dog-filter').textContent = 'Filter good dogs: OFF'
+    }
+}
 
+function fetchPups(filterOn) {
+    // console.log(filterOn)
     fetch('http://localhost:3000/pups')
         .then(res => res.json())
         .then(pups => {
             if (filterOn) {
-                const filtered = pups.filter(pup => pup.isGoodDog)
+                pups.filter(pup => pup.isGoodDog).forEach(pup => displayPup(pup))  
             } else {
                 pups.forEach(pup => displayPup(pup))
             }
@@ -25,53 +37,25 @@ function fetchPups(filterOn = false) {
 }
 
 function displayPup(pup) {
+  
     const span = document.createElement('span')
     span.textContent = pup.name
 
-    span.addEventListener('click', e => displayClickDog(pup))
+    span.addEventListener('click', e => displayDog(pup))
     pupsContainer.appendChild(span)
+    // console.log('pupContainer:',pupContainer)
 }
 
-
-function displayClickDog(pup) {
-    const img = document.createElement('img')
-    const nameH2 = document.createElement('h2')
-    const btn = document.createElement('button')
-    btn.id = 'dog'
-    img.src = pup.image
-    nameH2.textContent = pup.name
-    btn.textContent = pup.isGoodDog ? 'Good Dog!' : 'Bad Dog!'
-
-    btn.addEventListener('click', () => {
-        if (btn.textContent === 'Good Dog!') {
-            btn.textContent = 'Bad Dog!'
-        } else {
-            btn.textContent = 'Good Dog!'
-        }
-        pup.isGoodDog = pup.isGoodDog ? false : true
-        // update the backupend data
-        toggleDogBehaive(pup)
-    })
-
-    pupContainer.append(img, nameH2, btn)
-}
-    
-
-function pupHandler(pup) {
+function displayDog(pup) {
     pupContainer.innerHTML = ''
-     pup.isGoodDog = pup.isGoodDog ? false : true
-    toggleDogBehaive(pup)
-    console.log('call')
-}
-
-function displayDOg(pup) {
     const img = document.createElement('img')
     const nameH2 = document.createElement('h2')
     const btn = document.createElement('button')
     btn.id = 'dog'
     img.src = pup.image
     nameH2.textContent = pup.name
-    btn.textContent = pup.isGoodDog ? 'Good Dog!' : 'Bad Dog!'
+
+    btn.textContent = pup.isGoodDog ? "Good Dog!" : "Bad Dog!"
 
     btn.addEventListener('click', () => {
         if (btn.textContent === 'Good Dog!') {
@@ -79,26 +63,23 @@ function displayDOg(pup) {
         } else {
             btn.textContent = 'Good Dog!'
         }
+        pup.isGoodDog = pup.isGoodDog ? false : true 
+        updateDatabase(pup)
     })
 
     pupContainer.append(img, nameH2, btn)
-
-    console.log(pupContainer)
 }
 
-function toggleDogBehaive(pup) {
-    // console.log(`http://localhost:3000/pups/${pup.id}`)
-    fetch(`http://localhost:3000/pups/${pup.id}`, {
-        method: 'PATCH',
-        headers: {
-            'Content-Type': 'application/json',
-            Accept: 'application/json'
-        },
-        body: JSON.stringify(pup)
-    })
-        .then(res => res.json())
-        .then(pup => displayDOg(pup))
-
-    // .catch(error => console.error('Error PATCH:', error))
-    // e.target.textContent = pup.isGoodDog ? 'Goood Dog!' : 'Bad Dog!'
+function updateDatabase(pup) {
+   fetch(`http://localhost:3000/pups/${pup.id}`, {
+       method : 'PATCH',
+       headers : {
+           'Content-Type' : 'application/json',
+           Accept : 'application/json'
+       },
+       body : JSON.stringify(pup)
+   })
+   .then(res => res.json())
+   .then(pup => displayDog(pup))
+    console.log(pup)
 }
